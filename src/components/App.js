229a10be7049a0a5fdce1b8ddfa943a7d314898e
Ryan Gurnick <ryangurnick@gmail.com>
2023-10-24 20:43:09 -0700

    Episode 6

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import {useState} from "react";
 import '../reset.css';
 import '../App.css';
+import NoTodos from "./NoTodos";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
 
 function App() {
     const [todos, setTodos] = useState([
@@ -24,35 +27,22 @@ function App() {
         }
     ]);
 
-    const [todoInput, setTodoInput] = useState('');
     const [idForTodo, setIdForTodo] = useState(4);
 
-    function addTodo(event) {
-        // prevents the browser from submitting the form
-        event.preventDefault();
-
-        // check if the string is empty
-        if (todoInput.trim().length === 0)
-            return;
-
+    function addTodo(todo) {
         // ... copies the array
         setTodos([...todos, {
             id: idForTodo,
-            title: todoInput,
+            title: todo,
             isComplete: false
         }]);
 
-        setTodoInput('');
         setIdForTodo(prevIdForTodo => prevIdForTodo + 1);
         // setIdForTodo(idForTodo + 1);
     }
 
     function deleteTodo(id) {
         setTodos([...todos].filter(todo => todo.id !== id));
-    }
-
-    function handleInput(event) {
-        setTodoInput(event.target.value);
     }
 
     function completeTodo(id) {
@@ -98,78 +88,15 @@ function App() {
         <div className="todo-app-container">
             <div className="todo-app">
                 <h2>Todo App</h2>
-                <form action="#" onSubmit={addTodo}>
-                    <input
-                        type="text"
-                        value={todoInput}
-                        onChange={handleInput} // could also be onChange={(event) => { setTodoInput(event.target.value) }}
-                        className="todo-input"
-                        placeholder="What do you need to do?"
-                    />
-                </form>
+                <TodoForm addTodo={addTodo} />
 
-                <ul className="todo-list">
-                    {todos.map((todo, index) => (
-                        <li key={todo.id} className="todo-item-container">
-                            <div className="todo-item">
-                                <input type="checkbox" checked={todo.isComplete}
-                                       onChange={() => completeTodo(todo.id)}/>
-
-                                {!todo.isEditing ? (
-                                    <span
-                                        onDoubleClick={() => markAsEditing(todo.id)}
-                                        className={`todo-item-label ${todo.isComplete ? 'line-through' : ''}`}>{todo.title}</span>
-                                ) : (
-                                    <input type="text" defaultValue={todo.title} className="todo-item-input" autoFocus
-                                           onBlur={(event) => updateTodo(event, todo.id)}
-                                           onKeyDown={(event) => {
-                                               if (event.key === 'Enter')
-                                                   updateTodo(event, todo.id)
-                                               else if (event.key === 'Escape')
-                                                   markAsEditing(todo.id)
-                                           }}
-                                    />
-                                )}
-                            </div>
-                            <button onClick={() => deleteTodo(todo.id)} className="x-button">
-                                <svg
-                                    className="x-button-icon"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth={2}
-                                        d="M6 18L18 6M6 6l12 12"
-                                    />
-                                </svg>
-                            </button>
-                        </li>
-                    ))}
-                </ul>
-
-                <div className="check-all-container">
-                    <div>
-                        <div className="button">Check All</div>
-                    </div>
-
-                    <span>3 items remaining</span>
-                </div>
-
-                <div className="other-buttons-container">
-                    <div>
-                        <button className="button filter-button filter-button-active">
-                            All
-                        </button>
-                        <button className="button filter-button">Active</button>
-                        <button className="button filter-button">Completed</button>
-                    </div>
-                    <div>
-                        <button className="button">Clear completed</button>
-                    </div>
-                </div>
+                {
+                    todos.length > 0 ? (
+                        <TodoList todos={todos} completeTodo={completeTodo} markAsEditing={markAsEditing} updateTodo={updateTodo} cancelEdit={markAsEditing} deleteTodo={deleteTodo} />
+                    ) : (
+                        <NoTodos />
+                    )
+                }
             </div>
         </div>
     );
---
+++ src/components/TodoForm.js
@@ -0,0 +1,41 @@
+import React, {useState} from "react";
+import PropTypes from "prop-types";
+
+TodoForm.propTypes = {
+    addTodo: PropTypes.func.isRequired,
+}
+
+function TodoForm(props) {
+    const [todoInput, setTodoInput] = useState('');
+
+    function handleInput(event) {
+        setTodoInput(event.target.value);
+    }
+
+    function handleSubmit(event) {
+        // prevents the browser from submitting the form
+        event.preventDefault();
+
+        // check if the string is empty
+        if (todoInput.trim().length === 0)
+            return;
+
+        props.addTodo(todoInput);
+
+        setTodoInput('');
+    }
+
+    return (
+        <form action="#" onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={todoInput}
+                onChange={handleInput} // could also be onChange={(event) => { setTodoInput(event.target.value) }}
+                className="todo-input"
+                placeholder="What do you need to do?"
+            />
+        </form>
+    );
+}
+
+export default TodoForm
